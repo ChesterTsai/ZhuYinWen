4680117bbfdbb36dc3057e3c65903854961f673d
ChesterTsai <95360485+ChesterTsai@users.noreply.github.com>
2024-05-21 12:45:51 +0800

    Update ChuYingWeng.cpp
    remove the unnecessary tabs

--- ChuYingWeng.cpp
+++ ChuYingWeng.cpp
@@ -15,17 +15,17 @@ std::string wordReplace(char inputA, char inputB, std::string inputC, std::strin
 std::string changeToChuYing(std::string chuYingWeng){
 	std::string x = " 1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik,9ol.0p;/-";
 	std::string y[] = {" ", "ㄅ", "ㄆ", "ㄇ", "ㄈ", "ㄉ", "ㄊ", "ㄋ", "ㄌ", "ˇ", "ㄍ", "ㄎ", "ㄏ", "ˋ", "ㄐ", "ㄑ", "ㄒ", "ㄓ", "ㄔ", "ㄕ", "ㄖ", "ˊ", "ㄗ", "ㄘ", "ㄙ", "˙", "ㄧ", "ㄨ", "ㄩ", "ㄚ", "ㄛ", "ㄜ", "ㄝ", "ㄞ", "ㄟ", "ㄠ", "ㄡ", "ㄢ", "ㄣ", "ㄤ", "ㄥ", "ㄦ"};
-		
+
 	int lenOfInput = chuYingWeng.length();
 	int lenOfDecoder = x.length();
 	std::string decodedChuYing = "";
-	
+
 	for(int i = 0; i < lenOfInput; i++){
 		for(int j = 0; j < lenOfDecoder; j++){
 			decodedChuYing = wordReplace(chuYingWeng[i], x[j], y[j], decodedChuYing);
 		}
 	}
-	
+
 	return decodedChuYing;
 }
 
@@ -33,11 +33,11 @@ std::string changeToChuYing(std::string chuYingWeng){
 // std::string changeToPingYin(std::string chuYingWeng){
 // 	std::string x = " 1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik,9ol.0p;/-";
 // 	std::string y[] = {" ", "b", "p", "m", "f", "d", "t", "n", "l", " ", "g", "k", "h", " ", "j", "q", "x", "zh", "ch", "sh", "r", " ", "z", "c", "s", " ", "yi", "wu", "vu", "a", "o", "e", "e", "ai", "ei", "ao", "ou", "an", "in", "ang", "eng", "er"};
-	
+
 // 	int lenOfInput = chuYingWeng.length();
 // 	int lenOfDecoder = x.length();
 // 	std::string pinYing = "";
-	
+
 // 	for(int i = 0; i < lenOfInput; i++){
 // 		for(int j = 0; j < lenOfDecoder; j++){
 // 			pinYing = wordReplace(chuYingWeng[i], x[j], y[j], pinYing);
@@ -57,13 +57,13 @@ int main(){
 	std::getline(std::cin, chuYingWeng);
 
 	stolower(chuYingWeng);
-	
+
 	std::cout << "解譯結果：" << changeToChuYing(chuYingWeng) << "\n";
-	
+
 	// std::cout << "拼音" << changeToPingYin(chuYingWeng) << "\n";
-	
+
 	std::cout << "Press Enter Key To Continue...";
 	std::cin.get();
-	
+
 	return 0;
 }
