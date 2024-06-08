afb22a3a6fc49d8c61e61dfd7f4371cb1c09bad5
ChesterTsai <95360485+ChesterTsai@users.noreply.github.com>
2024-06-08 15:23:49 +0800

    Update ZhuYinWen.cpp
    - Make if-else statement in changeToPin() to switch-case statement, make it more readable(?) and run faster.
    - Add comments to the code to help people understand the code more.

--- ZhuYinWen.cpp
+++ ZhuYinWen.cpp
@@ -3,13 +3,18 @@
 // @version	3.0
 // @description	Convert weird numbers and signs to zhuYin
 // @source	https://github.com/ChesterTsai/ZhuYinWen.git
+// @lastUpdate	2024.06.08
 
 #include <iostream>
 #include <string>
 #include <algorithm>
 #include <map>
 
+
+// 轉換成注音
 std::string changeToZhuYin(std::string zhuYinWen){
+
+	// 英文及符號所對應的注音
 	std::map<char, std::string> mapForZhuYin = {
 		{' ', " "},
 		{'1', "ㄅ"},
@@ -55,6 +60,8 @@ std::string changeToZhuYin(std::string zhuYinWen){
 		{'-', "ㄦ"}
 	};
 
+
+	// 替換文字
 	int lenOfInput = zhuYinWen.length();
 	std::string decodedZhuYin = "";
 
@@ -66,9 +73,10 @@ std::string changeToZhuYin(std::string zhuYinWen){
 }
 
 
-// I started to hate myself after doing this part
-// why do I even do this shit
+// 轉換成拼音
 std::string changeToPinYin(std::string zhuYinWen){
+
+	// 一般情況下的拼音map
 	std::map<char, std::string> mapForPinYin = {
 		{' ', "-"},
 		{'1', "b"},
@@ -160,29 +168,37 @@ std::string changeToPinYin(std::string zhuYinWen){
 	std::string decodedPinYin = "";
 
 	for(int i = 0; i < lenOfInput; i++){
-		// 若聲母為"ㄧ"
-		if(zhuYinWen[i] == 'u'){
-			decodedPinYin += mapForU[zhuYinWen[i + 1]];
-			i += 1;
-		}
-		// 若聲母爲"ㄨ"
-		else if(zhuYinWen[i] == 'j'){
-			decodedPinYin += mapForJ[zhuYinWen[i + 1]];
-			i += 1;
-		}
-		//若聲母爲"ㄩ"
-		else if(zhuYinWen[i] == 'm'){
+
+		// 找出聲母
+		switch(zhuYinWen[i])
+		{
+		// 若聲母為"ㄩ"
+		case 'm':
 			decodedPinYin += mapForM[zhuYinWen[i + 1]];
 			i += 1;
-		}
-		else{
+			break;
+		// 若聲母爲"ㄨ"
+		case 'j':
+			decodedPinYin += mapForJ[zhuYinWen[i + 1]];
+			i += 1;
+			break;
+		// 若聲母爲"一"
+		case 'u':
+			decodedPinYin += mapForU[zhuYinWen[i + 1]];
+			i += 1;
+			break;
+		// 其他聲母
+		default:
 			decodedPinYin += mapForPinYin[zhuYinWen[i]];
+			break;
 		}
 	}
 
 	return decodedPinYin;
 }
 
+
+// 將字串轉換成小寫
 void stolower(std::string &s){
 	std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c){
 		return std::tolower(c);
@@ -198,8 +214,9 @@ int main(){
 
 	std::cout << "解譯結果：" << changeToZhuYin(zhuYinWen) << "\n";
 
-	std::cout << "拼音" << changeToPinYin(zhuYinWen) << "\n";
+	std::cout << "拼音：" << changeToPinYin(zhuYinWen) << "\n";
 
+	// Linux也可以用的暫停方法
 	std::cout << "Press Enter Key To Continue...";
 	std::cin.get();
 
